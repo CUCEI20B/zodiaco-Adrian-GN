8b675fcef05af7b73386b7f927397ef75c782d9e
Adrian Garcia <adrian.garcia2250@alumnos.udg.mx>
2020-09-28 19:30:41 -0500

    Prueba001

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hola Mundo")
+mes = int(input())
+dia = int(input())
+if  (31>= dia >=21 and mes ==3)or(1<= dia<=20 and mes==4):
+       print("aries")
+if (30>= dia >=21 and mes ==4)or(1<= dia<=20 and mes==5):
+       print("tauro")
+if  (31>= dia >=21 and mes ==5)or(1<= dia<=21 and mes==6):
+       print("geminis")
+if  (30>= dia >=22 and mes ==6)or(1<= dia<=22 and mes==7):
+       print("cancer")
+if (31>= dia >=23 and mes ==7)or(1<= dia<=22 and mes==8):
+       print("leo")
+if  (31>= dia >=23 and mes ==8)or(1<= dia<=22 and mes==9):
+       print("virgo")
+if (30>= dia >=23 and mes ==9)or(1<= dia<=22 and mes==10):
+       print("libra")
+if  (31>= dia >=23 and mes ==10)or(1<= dia<=22 and mes==11):
+       print("escorpio")
+if  (30>= dia >=23 and mes ==11)or(1<= dia<=21 and mes==12):
+       print("sagitario")
+if  (31>= dia >=22 and mes ==12)or(1<= dia<=20 and mes==1):
+       print("capricornio")
+if  (31>= dia >=21 and mes ==1)or(1<= dia<=18 and mes==2):
+       print("aquario")
+if  (29>= dia >=19 and mes ==2)or(1<= dia<=20 and mes==3):
+       print("pisis")
